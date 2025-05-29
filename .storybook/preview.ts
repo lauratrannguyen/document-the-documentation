7bc7a9217719b6d674a37bdebd52c77277d7fb58
Janelle Krupicka <jekrupicka@email.wm.edu>
2025-05-29 11:22:02 -0400

    Remove regex

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,6 @@ import { withThemeDecorator } from './preview-decorator'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
